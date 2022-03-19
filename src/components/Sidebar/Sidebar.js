86f38e8f65fab6e35f408452c68048113e94ce91
venln4 <rizqi.ZULFIAN@uobgroup.com>
2022-03-20 00:41:54 +0700

    [0011][feature] - Add Icon

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,16 +2,23 @@ import React from 'react';
 import "./Sidebar.css";
 import SidebarOption from './components/SidebarOption';
 import iconSpotifyLogo from '../../assets/img/spotify-logo.png';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <img className="sidebar__logo" height="50px" style={{padding: '20px', marginRight: 'auto'}} src={iconSpotifyLogo} alt="" />
-      <SidebarOption title="Home" />
-      <SidebarOption title="Search" />
-      <SidebarOption title="Your Library" />
+      <SidebarOption Icon={HomeIcon} title="Home" />
+      <SidebarOption Icon={SearchIcon} title="Search" />
+      <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+
+      <br />
+      <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr />
     </div>
-  )
+  );
 };
 
 export default Sidebar;
