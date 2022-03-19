f9e98b5e3adf58070f20a0ceed0e8ae86ddc945b
venln4 <rizqi.ZULFIAN@uobgroup.com>
2022-03-20 00:32:40 +0700

    [0010][feature] - Add SidebarOption & Material UI

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import "./Sidebar.css";
+import SidebarOption from './components/SidebarOption';
 import iconSpotifyLogo from '../../assets/img/spotify-logo.png';
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <img className="sidebar__logo" height="50px" style={{padding: '10px', marginRight: 'auto'}} src={iconSpotifyLogo} alt="" />
+      <img className="sidebar__logo" height="50px" style={{padding: '20px', marginRight: 'auto'}} src={iconSpotifyLogo} alt="" />
+      <SidebarOption title="Home" />
+      <SidebarOption title="Search" />
+      <SidebarOption title="Your Library" />
     </div>
   )
 };
