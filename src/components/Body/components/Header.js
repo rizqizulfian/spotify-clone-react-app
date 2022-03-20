33198b869478d2a56795c485426a3e3a711fcda0
venln4 <rizqi.ZULFIAN@uobgroup.com>
2022-03-20 23:45:57 +0700

    [0019][feature] - Pull user data from reducer

--- src/components/Body/components/Header.js
+++ src/components/Body/components/Header.js
@@ -3,8 +3,12 @@ import React from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import { Avatar } from '@mui/material';
 import "./Header.css";
+import { useDataLayerValue } from '../../../utils/DataLayer';
 
 const Header = () => {
+  const [{user }, dispatch] = useDataLayerValue();
+
+
   return (
     <div className="header">
       <div className="header__left">
@@ -16,8 +20,8 @@ const Header = () => {
       </div>
 
       <div className="header__right">
-        <Avatar src="" alt="RQ" />
-        <h4>Rafeh Qazi</h4>
+        <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
+        <h4>{user?.display_name}</h4>
       </div>
     </div>
   );
