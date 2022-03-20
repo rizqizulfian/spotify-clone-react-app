5db1521907371c1c78d99684ebdba3ca870907cd
venln4 <rizqi.ZULFIAN@uobgroup.com>
2022-03-20 14:59:29 +0700

    [0014][feature] - Add Footer content icon

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,31 @@
 import React from 'react';
+
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import RepeatIcon from '@mui/icons-material/Repeat';
+
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <h1>I am footer</h1>
+      <div className="footer__left">
+        <p>Album and song details</p>
+      </div>
+
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer_icon" />
+        <PlayCircleOutlineIcon className="" fontSize="large" />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__green" />
+      </div>
+
+      <div className="footer__right">
+        <p>Volume controls</p>
+      </div>
     </div>
   )
 };
