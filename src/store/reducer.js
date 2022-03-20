94d4ef1841925e55e55ce00488d277ea5fcbde71
venln4 <rizqi.ZULFIAN@uobgroup.com>
2022-03-21 00:25:01 +0700

    [021][feature] - Pull data discover weekly reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,7 +4,7 @@ export const initialState = {
   playing: false,
   item: null,
   // remove token to be null after finish developing
-  token: 'BQAtQRNR4Viorze3DSfMgalyms3a4_MfYv2zbUin8w3746WWtFy_xPEHPd6azn_oTIwLJZyRw5kBj4g0bAyCzSRPKRMzAkZuDa9sFW8pGJnRM8PnVt0P-y33lc-gVobHKVr6BqbOpq3rCiYMRVmK4LhcyNR_DJBOGYV-MEoYLZCLPIGBfJ_tLffMBNpZYXjyB9piWvGDl0pLBh4wyOwE-o8ccw9KkHiVJk59AY-hOeE0dfhBcGLURvjhG0m3daMKtxkUewdL_A6NJSN_eyP5XsBRznZ1LNtQYXs',
+  // token: 'BQAtQRNR4Viorze3DSfMgalyms3a4_MfYv2zbUin8w3746WWtFy_xPEHPd6azn_oTIwLJZyRw5kBj4g0bAyCzSRPKRMzAkZuDa9sFW8pGJnRM8PnVt0P-y33lc-gVobHKVr6BqbOpq3rCiYMRVmK4LhcyNR_DJBOGYV-MEoYLZCLPIGBfJ_tLffMBNpZYXjyB9piWvGDl0pLBh4wyOwE-o8ccw9KkHiVJk59AY-hOeE0dfhBcGLURvjhG0m3daMKtxkUewdL_A6NJSN_eyP5XsBRznZ1LNtQYXs',
 };
 
 const reducer = (state, action) => {
@@ -16,7 +16,7 @@ const reducer = (state, action) => {
       }
     case 'SET_TOKEN':
       return {
-        ...state,
+        ...state, 
         token: action.token,
       }
 
@@ -24,6 +24,12 @@ const reducer = (state, action) => {
       return {
         ...state,
         playlists: action.playlists,
+      }
+
+    case 'SET_DISCOVER_WEEKLY':
+      return {
+        ...state,
+        discover_weekly: action.discover_weekly,
       }
     default:
       return state;
