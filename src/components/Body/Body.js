5af5f58b9970715cef8fb0aa9380296de61a3ae5
venln4 <rizqi.ZULFIAN@uobgroup.com>
2022-03-20 23:53:22 +0700

    [0020][feature] - Add discover weekly

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -6,8 +6,17 @@ import "./Body.css";
 const Body = ({ spotify }) => {
   return (
     <div className="body">
-      <h1>I am the body</h1>
       <Header spotify={spotify} />
+
+      <div className="body__info">
+        {/* <img src="https://i.scdn.co/image/6682aad217c1" alt="" /> */}
+        <img src="https://cdns-images.dzcdn.net/images/cover/cb3c5907fc103288135db863a70d66f7/500x500.jpg" alt="" />
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>Description</p>
+        </div>
+      </div>
     </div>
   )
 };
