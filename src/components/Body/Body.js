f92df9ce2b5385035313c43818bd97169fd14663
venln4 <rizqi.ZULFIAN@uobgroup.com>
2022-03-22 23:38:46 +0700

    [022][feature] - Checkpoint Playing music

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
-
-import "./Body.css";
+import SongRow from './components/SongRow';
 import { useDataLayerValue } from '../../utils/DataLayer';
 
-const Body = ({ spotify }) => {
-  const [{discover_weekly}, dispatch] = useDataLayerValue();
+import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import "./Body.css";
 
+const Body = ({ spotify }) => {
+  const [{ discover_weekly }, dispatch] = useDataLayerValue();
+  console.log('ini masuk dis', discover_weekly)
   return (
     <div className="body">
       <Header spotify={spotify} />
@@ -18,6 +22,18 @@ const Body = ({ spotify }) => {
           <h2>Discover Weekly</h2>
           <p>{discover_weekly?.description}</p>
         </div>
+      </div>
+
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilledIcon className="body__shuffle" />
+          <FavoriteIcon fontSize="large" />
+          <MoreHorizIcon />
+        </div>
+
+        {discover_weekly?.tracks.items.map(item => (
+          <SongRow track={item.track} />
+        ))}
       </div>
     </div>
   )
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
-import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
-import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
-import SkipNextIcon from '@mui/icons-material/SkipNext';
-import ShuffleIcon from '@mui/icons-material/Shuffle';
-import RepeatIcon from '@mui/icons-material/Repeat';
-import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
-import VolumeDownIcon from '@mui/icons-material/VolumeDown';
-import { Grid, Slider } from '@mui/material';
+// import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+// import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+// import SkipNextIcon from '@mui/icons-material/SkipNext';
+// import ShuffleIcon from '@mui/icons-material/Shuffle';
+// import RepeatIcon from '@mui/icons-material/Repeat';
+// import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
+// import VolumeDownIcon from '@mui/icons-material/VolumeDown';
+import AudioPlayer from 'react-h5-audio-player';
+// import { Grid, Slider } from '@mui/material';
 
 import "./Footer.css";
+import 'react-h5-audio-player/lib/styles.css';
+
 
 const Footer = () => {
   return (
@@ -23,15 +26,27 @@ const Footer = () => {
       </div>
 
       <div className="footer__center">
-        <ShuffleIcon className="footer__green" />
+        {/* <ShuffleIcon className="footer__green" />
         <SkipPreviousIcon className="footer__icon" />
         <PlayCircleOutlineIcon className="" fontSize="large" />
         <SkipNextIcon className="footer__icon" />
-        <RepeatIcon className="footer__green" />
+        <RepeatIcon className="footer__green" /> */}
+        <div id="player">
+          <div className="track-slider">
+            <AudioPlayer
+              src="https://p.scdn.co/mp3-preview/56234438d41c69c8d81ad36f80a5d4366664b243?cid=1fd77f4a31ea4e059596f9fcb3a6af41"
+              customAdditionalControls={[]}
+              layout="stacked-reverse"
+              // autoPlay
+              volume={0.5}
+              showDownloadProgress={false}
+            />
+          </div>
+        </div>
       </div>
 
       <div className="footer__right">
-        <Grid container spacing={2}>
+        {/* <Grid container spacing={2}>
           <Grid item>
             <PlaylistPlayIcon />
           </Grid>
@@ -41,7 +56,7 @@ const Footer = () => {
           <Grid item xs>
             <Slider />
           </Grid>
-        </Grid>
+        </Grid> */}
       </div>
     </div>
   )
